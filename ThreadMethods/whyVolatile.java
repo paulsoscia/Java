4c0d3f144ed88e676a1d5d49fda8ad8812f437f3
paul soscia <paulsoscia@gmail.com>
2024-03-15 21:33:03 -0400

    Create whyVolatile.java

---
+++ ThreadMethods/whyVolatile.java
@@ -0,0 +1,45 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+class Processor extends Thread {
+
+	private volatile boolean running = true;
+	
+	
+	public void run() {
+		
+		while (running) {
+			System.out.println("From child thread (extends Thread class) Hello");
+			
+			try {
+				Thread.sleep(600);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void shutdown() {
+		running = false;
+	}
+}
+
+public class whyVolatile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main BEGIN");
+		Processor proc1 = new Processor();
+		proc1.start();
+		
+		System.out.println("Press return to stop ...");
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+		
+		proc1.shutdown();
+ 
+		System.out.println("main END");
+	}
+
+}
